3642e7212a92bcd21b51c73ace6057acd3d28d79
hunkid <273946286@qq.com>
2017-10-16 15:47:31 +0800

    更新可供直接访问的static

--- src/constants/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const CHANGE_PIECE_POS = 'Change_Piece_Pos'
-export const ADD_PIECE = 'ADD_PIECE'
-export const DEL_PIECE = 'DEL_PIECE'
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
-
-import reducer from '../reducers'
-
-export const store = createStore(reducer, applyMiddleware(createLogger()))
